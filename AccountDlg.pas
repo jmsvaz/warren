a917d72948bc054691cde4ad00c60b5c4faaafba
João Marcelo <jmsvaz@gmail.com>
2012-02-11 14:58:08 -0200

    Account Editor Dialog created

--- AccountDlg.pas
+++ AccountDlg.pas
@@ -5,10 +5,30 @@ unit AccountDlg;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls, EditBtn, Spin, Buttons;
 
 type
+
+  { TAccountEditorDialog }
+
   TAccountEditorDialog = class(TForm)
+    btCancel: TBitBtn;
+    btHelp: TBitBtn;
+    btOK: TBitBtn;
+    cbAccountType: TComboBox;
+    cbCurrency: TComboBox;
+    cbFavorite: TCheckBox;
+    cbInactive: TCheckBox;
+    dtInitialDate: TDateEdit;
+    edAccountName: TLabeledEdit;
+    edInitialBalance: TFloatSpinEdit;
+    lbAccountType: TLabel;
+    lbCurrency: TLabel;
+    lbInitialBalance: TLabel;
+    lbInitialDate: TLabel;
+    lbNotes: TLabel;
+    mmNotes: TMemo;
   private
     { private declarations }
   public
